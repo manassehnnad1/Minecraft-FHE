404e28e64a0e74579a88ed12e1e66a8d5bd9d491
Manasseh <judithnnadi98@gmail.com>
2025-09-22 17:17:50 +0100

    hardhat ether deps

--- src/components/ui/Signature.tsx
+++ src/components/ui/Signature.tsx
@@ -2,7 +2,7 @@
 
 const Signature = () => {
   return (
-  <a href="https://github.com/manassehnnd1">
+  <a href="https://github.com/manassehnnad1">
     <p className="font-tektur text-white cursor-pointer">M7-Isla</p>
   </a>
 
